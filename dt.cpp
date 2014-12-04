a2a2eb4ee25baeb939c284e81aa99e20a272c24e
Ian Forbes <ian.forbes@mail.mcgill.ca>
2014-12-04 16:08:10 -0500

    Update

--- dt.cpp
+++ dt.cpp
@@ -1,11 +1,30 @@
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <locale>
+#include <map>
 #include <string>
 #include <vector>
-#include <algorithm>
 
 #include <boost/filesystem.hpp>
 
 #include <opencv2/opencv.hpp>
 
+struct colon_is_space : std::ctype<char>
+{
+    colon_is_space() : std::ctype<char>(get_table()) {}
+
+    static mask const* get_table() {
+        static mask rc[table_size];
+        rc[','] = std::ctype_base::space;
+        rc['\n'] = std::ctype_base::space;
+        return &rc[0];
+    }
+};
+
+
+  
+using std::locale;
 namespace fs = boost::filesystem;
 
 cv::Mat dt(size_t pos, const std::vector<cv::Mat>& seq, int CV_TYPE = CV_32F){
@@ -34,29 +53,45 @@ int main(int argc, char* argv[]) {
         leftoff = fs::path(argv[2]); // last video which was labeled. Allows resuming.
     }
     std::vector<fs::path> results;
+    std::map<std::string,int> labels;
+    
+    std::fstream fin("labels.csv");
+    fin.imbue(locale(fin.getloc(), new colon_is_space));
+    while(1){
+        int label = 5;
+        std::string temp;
+        fin >> temp >> label;
+        if(!fin.good()) break; 
+        temp.pop_back();
+        temp.pop_back();
+        temp.pop_back();
+        temp.pop_back();
+        labels[temp] = label;
+        std::cout << temp << " --> " << label << std::endl; 
+    }
 
     // Get filenames for videos in dir
+    
     if(fs::exists(someDir) && fs::is_directory(someDir)) {
         for(fs::directory_iterator dir_iter(someDir) ; dir_iter != end_iter ; ++dir_iter) {
             if(fs::is_regular_file(dir_iter->status()) ) {
-              results.emplace_back( *dir_iter);
+              results.emplace_back(*dir_iter);
             }
         }
     }
 
+    std::ofstream fout("./manifest.info");
+
     bool skip = (argv[2] != nullptr);
     cv::namedWindow("Cars"); // Create a window called 'Cars'
     std::sort(results.begin(), results.end()); 
+    const cv::Rect ROI(150, 50, 80, 165); 
     for(auto& filename : results) {
         if(skip){
             skip = (filename != leftoff);
             continue;
         }
-        std::cout << filename << "  = " << std::flush;
-        std::cerr << filename << "  = " << std::flush;
         cv::VideoCapture vid(filename.string()); // Open filename
-        int z = 0;
-        cv::Rect ROI(140, 50, 100, 160);
         cv::Mat frame;
         std::vector<cv::Mat> traffic;
 
@@ -68,16 +103,35 @@ int main(int argc, char* argv[]) {
         }
         for(size_t i = 0; i < traffic.size(); i++){
             cv::Mat deriv = dt(i, traffic);
-            cv::normalize(deriv, deriv, 0, 1.0, cv::NORM_MINMAX, CV_32FC1); //normalize on [0,1]
-            //cv::GaussianBlur(deriv, deriv, cv::Size(5,5), 3);
-            cv::imshow("Cars", deriv); // display the last video frame into the cars window.
-            z = cv::waitKey(1000.0/15.0); // wait for a period of time that makes the video run at 15fps.
+            double min, max;
+            cv::minMaxIdx(deriv, &min, &max);
+            deriv = (deriv - min);
+            //cv::threshold(deriv, deriv, 255, 255, cv::THRESH_TRUNC);
+            cv::minMaxIdx(deriv, &min, &max);
+            // std::cout << max << std::endl;
+            // cv::normalize(deriv, deriv, 0, 255, cv::NORM_MINMAX, CV_8UC1); //normalize on [0,1]
+            // cv::threshold(deriv, deriv, 0.75, 1.0, cv::THRESH_TOZERO);
+            // cv::imshow("Cars", deriv); // display the last video frame into the cars window.
+            // cv::waitKey(50);
+            std::string outfile("./all/");
+            const std::string basename = fs::basename(filename);
+            const std::string dirless = basename + ":" + std::to_string(i) + ".png";
+            outfile += dirless;
+            int label = 0;
+            if( labels.find(basename) == labels.end())
+                std::cout << "Error" << std::endl;
+            else {
+                label = labels[basename];
+                if(max < 0.21 && label < 4) label = 0;
+                std::cout << outfile << " " << label << std::endl;
+            }
+            bool ret = cv::imwrite(outfile, 255.0 * deriv);
+            if(!ret)
+                std::cout << outfile << std::endl;
+            else
+                fout << dirless << ' ' << label << std::endl;
+            //z = cv::waitKey(1000.0/15.0); // wait for a period of time that makes the video run at 15fps.
         }
-        if(z <= 0) {
-            z = cv::waitKey(1000*100); // wait for label after showing video
-        }
-        std::cout << static_cast<char>(z) << std::endl;
-        std::cerr << static_cast<char>(z) << std::endl;
     }
     return 0;
 }
